b01b8878e23f6a83421331f721ca3641ae69cc27
Mohankumae9234 <mohankumarsl2005@gmail.com>
2026-09-02 21:09:20 +0530

    2-9-26 i complete entity,service

---
+++ src/main/java/org/example/service/userBookingService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.entity.User;
+
+public class userBookingService {
+    private User user;
+
+    public userBookingService(User user) {
+        this.user = user;
+    }
+}
